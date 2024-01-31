482d65e5c6c41771d100fa2b2209a8ba8243f6fa
HossainIdrish <139071336+HossainIdrish@users.noreply.github.com>
2024-01-30 19:07:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,52 @@
-    require([
-      "esri/Map",
-      "esri/layers/CSVLayer",
-      "esri/views/MapView",
-      "esri/config",
-      "esri/core/urlUtils",
-      "dojo/domReady!"
-    ], function(
-      Map,
-      CSVLayer,
-      MapView,
-      esriConfig,
-      urlUtils
-    ) {
+require([
+  "esri/Map",
+  "esri/layers/CSVLayer",
+  "esri/views/MapView",
+  "esri/config",
+  "esri/core/urlUtils",
+  "dojo/domReady!"
+], function(
+  Map,
+  CSVLayer,
+  MapView,
+  esriConfig,
+  urlUtils
+) {
+  var url = "https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%202/stl_crime_wgs_84.csv";
+  esriConfig.request.corsEnabledServers.push('https://raw.githubusercontent.com');
 
-      // If CSV files are not on the same domain as your website, a CORS enabled server
-      // or a proxy is required.
-     var url = "https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%202/stl_crime_wgs_84.csv";
-     esriConfig.request.corsEnabledServers.push('https://rawgit.com');
+  // Update the template to match the fields in your CSV file
+  const template = {
+    title: "Crime Info",
+    content: "Details about the crime event."
+    // Customize the content with appropriate fields from your CSV
+  };
 
-      // Paste the url into a browser's address bar to download and view the attributes
-      // in the CSV file. These attributes include:
-      // * mag - magnitude
-      // * type - earthquake or other event such as nuclear test
-      // * place - location of the event
-      // * time - the time of the event
+  const csvLayer = new CSVLayer({
+    url: url,
+    popupTemplate: template
+  });
 
-        const template = {
-          title: "Earthquake Info",
-          content: "Magnitude {mag} {type} hit {place} on {time}."
-        };
+  var symbol = {
+    type: "simple-marker", 
+    color: "red"
+  };
 
-        const csvLayer = new CSVLayer({
-          url: url,
-          copyright: "USGS Earthquakes",
-          popupTemplate: template
-        });
+  csvLayer.renderer = {
+    type: "simple",
+    symbol: symbol
+  };
 
-        var symbol = {
-          type: "simple-marker", 
-          color: "purple"
-        };
+  var map = new Map({
+    basemap: "gray",
+    layers: [csvLayer]
+  });
 
-      csvLayer.renderer = {
-        type: "simple", // autocasts as new SimpleRenderer()
-        symbol: symbol
-      };
+  var view = new MapView({
+    container: "viewDiv",
+    center: [-90.1994, 38.6270],
+    zoom: 10,
+    map: map
+  });
+});
 
-      var map = new Map({
-        basemap: "gray",
-        layers: [csvLayer]
-      });
-
-      var view = new MapView({
-        container: "viewDiv",
-        center: [-90.1994, 38.6270],
-        zoom: 10,
-        map: map
-      });
-
-    });
